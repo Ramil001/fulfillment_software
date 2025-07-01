9b49c532cc963165fb7918d2143ee007361a6a8c
root <ramil.belevtzov@gmail.com>
2025-07-01 17:17:07 +0000

    Change logic

--- models/warehouses.py
+++ models/warehouses.py
@@ -5,7 +5,12 @@ class FulfillmentWarehouses(models.Model):
     _inherit = 'stock.warehouse'
     
     is_fulfillment = fields.Boolean(string="Is this a fulfillment warehouse?")
-    linked_partner_id = fields.Many2one(
+    fulfillment_creator_id = fields.Char(string="Fulfillment created Id")
+    fulfillment_owner_id = fields.Many2one(
      'fulfillment.partners',
      string="Linked fulfillment partner"
     )
+
+   
+    
+    
