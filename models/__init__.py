c5148d71efe7418bd19c712329f360a4288e97e6
root <ramil.belevtzov@gmail.com>
2025-07-16 13:07:17 +0000

    Added in maintanse

--- models/__init__.py
+++ models/__init__.py
@@ -5,3 +5,4 @@ from . import partners
 from . import profile
 from . import warehouses
 from . import products
+from . import purchase
--- models/purchase.py
+++ models/purchase.py
@@ -3,19 +3,13 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
-class PurchaseOrder(models.Model):
+class FulfillmentPurchase(models.Model):
     _inherit = 'purchase.order'
 
     @api.model
-    def web_save(self, vals, specification=None):
-        _logger.warning(f"[web_save OVERRIDE] {vals}")
-        return super().web_save(vals, specification)
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        _logger.warning(f"[create OVERRIDE] {vals_list}")
-        return super().create(vals_list)
+  
+    def create(self, vals):
+        _logger.info(f"[create OVERRIDE]: {vals}")
 
     def write(self, vals):
-        _logger.warning(f"[write OVERRIDE] {vals}")
-        return super().write(vals)
+        _logger.warning(f"[write OVERRIDE]: {vals}")
