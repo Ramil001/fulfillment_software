dcc63e909e5d9208de17e056983fac25845b6040
root <ramil.belevtzov@gmail.com>
2025-07-16 12:43:09 +0000

    Added purchase log

--- models/purchase.py
+++ models/purchase.py
@@ -1,8 +1,21 @@
-from odoo import models, fields, api
+from odoo import models, api
 import logging
-from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
-class FulfillmentPurchase(models.Model):
+class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
+
+    @api.model
+    def web_save(self, vals, specification=None):
+        _logger.warning(f"[web_save OVERRIDE] {vals}")
+        return super().web_save(vals, specification)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        _logger.warning(f"[create OVERRIDE] {vals_list}")
+        return super().create(vals_list)
+
+    def write(self, vals):
+        _logger.warning(f"[write OVERRIDE] {vals}")
+        return super().write(vals)
