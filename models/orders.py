774d74d8260ee9e2acf8cb61b09025c546d3623b
root <ramil.belevtzov@gmail.com>
2025-11-03 14:01:49 +0000

    [IMP] Added notebook tab in order and added order external id

--- models/orders.py
+++ models/orders.py
@@ -1,10 +1,18 @@
 from odoo import models, fields
 
+
+class FulfillmentOrder(models.Model):
+    _inherit = 'sale.order'
+    
+    fulfillment_order_id = fields.Char(string="Fulfillment Order ID", readonly=True)
+         
+     
+
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     fulfillment_partner_id = fields.Many2one(
         'fulfillment.partners',
-        string='Fulfillment Partner',
+        string='Fulfillment Delivery',
         help='Кто отправляет этот товар',
     )
