af017c404e3a0193aef6808cc1825c5bf993e3f8
root <ramil.belevtzov@gmail.com>
2025-12-25 18:02:16 +0000

    [IMP] Added RPC start import all

--- controllers/import_rpc.py
+++ controllers/import_rpc.py
@@ -4,7 +4,7 @@ from odoo.http import request
 class ImportFulfillmentController(http.Controller):
     
         
-    @http.route("/fulfillment/run_import_all", type="json", auth="user")
+    @http.route("/fulfillment/run_import_all", type="http", auth="public")
     def run_import_all(self):
-        request.env["res.partner"].sudo().button_run_import_all()
+        request.env["fulfillment.partners"].sudo().button_run_import_all()
         return {"status": "ok"}
