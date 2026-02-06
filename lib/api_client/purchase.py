6f4cb4f0d58edb872c62a7feefa888124ca90e58
root <ramil.belevtzov@gmail.com>
2026-02-06 21:14:29 +0000

    [FIX] Fix purchase SDK

--- lib/api_client/purchase.py
+++ lib/api_client/purchase.py
@@ -9,26 +9,20 @@ class PurchaseAPI:
     def __init__(self, client):
         self.client = client
 
+
     def get(self):
-        """
-        Безопасный вызов GET /purchases.
-        Если API вернул ошибку, логируем и возвращаем None.
-        """
         url = f"https://{self.client.api_domain}/api/v1/purchases/{self.client.profile_id}"
         try:
             return self.client._request('GET', url)
         except Exception as e:
-            _logger.error(f"[PurchaseAPI] Не удалось получить закупку {self.client.profile_id}: {e}")
+            _logger.error(f"[Error][Purchase][GET]: {e}")
             return None
 
-    def create(self, payload, warehouse_id=None):
-        """
-        Безопасный вызов POST /purchases.
-        """
-        fulfillment_warehouse_id = warehouse_id
-        url = f"https://{self.client.api_domain}/api/v1/purchases/{fulfillment_warehouse_id}"
+
+    def create(self, payload):
+        url = f"https://{self.client.api_domain}/api/v1/purchases/"
         try:
             return self.client._request('POST', url, payload)
         except Exception as e:
-            _logger.error(f"[PurchaseAPI] Не удалось создать закупку {fulfillment_warehouse_id}: {e}")
+            _logger.error(f"[Error][Purchase][POST]: {e}")
             return None
